1a969a5b2394360c4cfbbbcfa805cbd669a5c2c5
Emrullah <emrullahucel@gmail.com>
2024-04-23 20:52:33 +0300

    added admin page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,11 @@
+import { NavLink } from "react-router-dom";
 import "./footer.css";
 const Footer = () => {
   return (
     <footer>
       <h3>Qr menu için iletişim</h3>
       <p>emrullahucel@gmail.com</p>
+      <NavLink to="/admin">admin paneli</NavLink>
     </footer>
   );
 };
